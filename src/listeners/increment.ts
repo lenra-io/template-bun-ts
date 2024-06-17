478f4b260c946e1f2c6cc1a23a1a79ac64baffd0
jonas.martinez <cbobbxta.fr@gmail.com>
2024-06-17 10:23:15 +0200

    add secret log

--- src/listeners/increment.ts
+++ src/listeners/increment.ts
@@ -2,6 +2,9 @@ import { Api, ListenerRequest } from '@lenra/app';
 import { Counter } from '../classes/Counter.js';
 
 export default async function (props: { id: string }, _event: ListenerRequest['event'], api: Api) {
+    console.log("I found your secret !!")
+    console.log(process.env.REALLY_SECRET);
+
     await api.data.coll(Counter).updateMany({ _id: props.id }, {
         $inc: {
             count: 1
